8a7b76de533559862fbd58418c80f2e933ad6387
daw883 <dawei_bsfl@qq.com>
2022-07-24 16:30:37 +0800

    COMMIT

---
+++ src/com/opengood/s1/KtBase07.kt
@@ -0,0 +1,19 @@
+package com.opengood.s1
+
+fun main() {
+    print("hello world : ")
+    /*可读可写 名称 类型 值 */
+    val name : String = "kevin"
+    print(name)
+    /*
+    * String 字符串
+    * Char 字符
+    * Boolean 布尔型
+    * Int 整型
+    * Double 小数
+    * List 集合
+    * Set 无重复元素集合
+    * Map 键值对集合
+    * */
+}
+SS
---
+++ src/com/opengood/s1/KtBase08.kt
@@ -0,0 +1,10 @@
+package com.opengood.s1
+
+fun main() {
+    val name : String = "kevin"
+    print(name)
+
+    var age : Int  = 10
+    age = 11
+    print(age)
+}
